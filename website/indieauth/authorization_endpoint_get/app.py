3c478b5e6d39f4277d6844381cc79cfba39b540a
Ross Karchner <ross@karchner.com>
2021-05-19 08:27:06 -0400

    Pre-vacation checkin 2

--- website/indieauth/authorization_endpoint_get/app.py
+++ website/indieauth/authorization_endpoint_get/app.py
@@ -5,59 +5,78 @@ import chevron
 import hmac
 import time
 import os
-
-from urllib.parse import urlparse as _urlparse
-
+import secrets
 import boto3
 
-secrets = boto3.client("secretsmanager")
+
+from urllib.parse import urlencode, urlparse as _urlparse
 
 urlparse = functools.lru_cache(_urlparse)
 
-# by populating these at the module level, we create the potential
-# for it to fall briefly out of sync after a secret rotation. Because all subsequent
-# requests will use the VersionId, I think this is OK.
-secret_data = secrets.get_secret_value(SecretId=os.environ["HMAC_SECRET_ARN"])
-hmac_key = json.loads(secret_data["SecretString"])["key"]
-hmac_key_version = secret_data["VersionId"]
+s3 = boto3.resource("s3")
+session_bucket = s3.Bucket(os.environ["SESSION_BUCKET"])
+continuation_bucket = s3.Bucket(os.environ["CONTINUATION_BUCKET"])
 
 
-def is_valid_url(url, allow_non_http=False):
-    parsed_url = urlparse(url)
-    protocol_ok = allow_non_http or parsed_url.scheme.lower() in ["http", "https"]
-    return bool(protocol_ok and parsed_url.netloc)
+def session_data(event):
+    for cookie in event.get("cookies", []):
+        if cookie.startswith("session="):
+            session_key = cookie[8:]
+            session_raw = (
+                session_bucket.Object(session_key).get()["Body"].read().decode("utf-8")
+            )
+            return session_key, json.loads(session_raw)
 
 
-def lambda_handler(event, context):
-    params = event["queryStringParameters"]
+def redirect_to_cognito(event):
+        params = event["queryStringParameters"]
+        continuation_data = params.copy()
+        continuation_key = secrets.token_hex()
 
-    if all(
-        [
-            is_valid_url(params["client_id"]),
-            is_valid_url(params["redirect_uri"], allow_non_http=True),
-            ("me" not in params or is_valid_url(params["me"])),
-        ]
-    ):
-        now = time.time()
-        query_string = event["rawQueryString"]
-        upstream_sub_id = event["pathParameters"]["upstream_sub_id"]
-        message = upstream_sub_id + query_string + str(now)
-        hashed = hmac.new(
-            hmac_key.encode("utf-8"), message.encode("utf-8"), "sha256"
-        ).hexdigest()
-        csrf_token = "%s@%s@%s" % (hashed, hmac_key_version, now)
+        continuation_bucket.put_object(
+            Key=continuation_key, Body=json.dumps(continuation_data)
+        )
 
-        template_context = {
-            "query_string": base64.b64encode(bytes(query_string, encoding="utf-8")).decode("utf-8"),
-            "upstream_sub_id": upstream_sub_id,
-            "csrf_token": csrf_token,
-            "client_id": params["client_id"],
-            "scope": params.get("scope"),
+        upstream_oauth_args = {}
+        upstream_oauth_args["state"] = continuation_key
+        upstream_oauth_args["response_type"] = "code"
+        upstream_oauth_args["client_id"] = os.environ["USER_POOL_CLIENT"]
+        upstream_oauth_args["redirect_uri"] = (
+            "https://%s/callback" % event["headers"]["host"]
+        )
+
+        upstream_oauth_args["scope"] = "https://indieauth.spec.indieweb.org/login"
+
+        # redirect to cognito
+
+        return {
+            "statusCode": 302,
+            "headers": {
+                "Location": os.environ["UPSTREAM_OAUTH_AUTHORIZE"]
+                + "?"
+                + urlencode(upstream_oauth_args)
+            },
         }
 
-        with open("get.html") as template:
-            return {
-                "statusCode": 200,
-                "headers": {"Content-Type": "text/html"},
-                "body": chevron.render(template=template, data=template_context),
-            }
+def display_confirmation_screen(event, session_key, session):
+    params = event["queryStringParameters"]
+
+
+def csrf_token(session_key, session):
+    now=time.time()
+    message="%s@%s" %(session_key, str(now)).encode('utf-8')
+    key=session['secret'].encode('utf-8')
+    hasher=hmac.new(key, message, digestmod='sha256')
+    return "%s@%s" % (hasher.hexdigest(), str(now))
+
+def lambda_handler(event, context):
+
+    session_key, session = session_data(event)
+
+    if (
+        session
+        and session["claims"]["sub"] == event["pathParameters"]["upstream_sub_id"]
+    ):
+        print("now what?")
+    else:
+        return redirect_to_cognito(event)
